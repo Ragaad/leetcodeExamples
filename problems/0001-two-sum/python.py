d5f68878c9972833f1a03d5b5bf0c4514ec747fc
mahy777-bit <82967240+mahy777-bit@users.noreply.github.com>
2025-10-23 16:23:26 -0700

    Create python.py

---
+++ problems/0001-two-sum/python.py
@@ -0,0 +1,9 @@
+
+class Solution:
+    def twoSum(self, nums: List[int], target: int) -> List[int]:
+        visited={}
+        for i,num in enumerate(nums):
+            missing=target-num
+            if missing in visited:
+                return [visited[missing],i]
+            visited[num]=i
