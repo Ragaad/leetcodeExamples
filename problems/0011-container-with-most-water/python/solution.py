fbe97259dca6d14411d138f9c45edaca7c8ea875
mahy777-bit <maha.abdelraosul@gmail.com>
2025-11-20 10:18:19 -0800

    Python Solution and Test for Container with Most Water

--- problems/0011-container-with-most-water/python/solution.py
+++ problems/0011-container-with-most-water/python/solution.py
@@ -1,12 +1,26 @@
-# TODO: implement the Python version for this problem.
+from typing import List
 
 class Solution:
-    def placeholder(self):
-        pass
+    def maxArea(self, height: List[int]) -> int:
+        start=0
+        end=len(height)-1
+        width=end
+        max_area=float('-inf')
+        while start<end:
+            length=min(height[start],height[end])
+            max_area=max(max_area,width*length)
+            if height[start]<height[end]:
+                start+=1
+            else:
+                end-=1
+            width-=1
+        return max_area
+
 
 def solve():
-    # optional runner entrypoint
-    pass
+    sol=Solution()
+    hieghts=[1,8,6,2,5,4,8,3,7]
+    print(f"The max area is {sol.maxArea(hieghts)}")
 
 if __name__ == "__main__":
-    print("Implement me!")
+    solve()
--- problems/0011-container-with-most-water/python/test_solution.py
+++ problems/0011-container-with-most-water/python/test_solution.py
@@ -1,3 +1,25 @@
-# pytest skeleton. Replace with real tests.
-def test_placeholder():
-    assert True
+import unittest
+from solution import Solution
+class TestMergedIntervals(unittest.TestCase):
+    def setUp(self):
+        self.sol=Solution()
+    
+    def check(self,height,expected):
+        got=self.sol.maxArea(height)
+        self.assertEqual(got,expected,f"For the heights {height} got {got} but expected {expected}")
+        return got == expected
+    
+    def testExamples(self):
+        tests=[
+            ([1,8,6,2,5,4,8,3,7],49),
+            ([1,1],1)
+        ]
+
+        passed=0
+        for test in tests:
+            if self.check(test[0],test[1]):
+                passed+=1
+        print(f"\nSummary: {passed}/{len(tests)} ")
+
+if __name__=="__main__":
+    unittest.main()
