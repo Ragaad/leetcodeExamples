01a8eb004c65ac71df549d7e38f6314b5cccba2a
mahy777-bit <maha.abdelraosul@gmail.com>
2025-11-19 19:10:52 -0800

    Python Solution and Test for Course Schedule Problem

--- problems/0207-course-schedule/python/solution.py
+++ problems/0207-course-schedule/python/solution.py
@@ -1,13 +1,35 @@
-def solve(*args, **kwargs):
-    """
-    TODO: implement Python solution for this problem.
+from typing import List
+class Solution:
+    def canFinish(self, numCourses: int, prerequisites: List[List[int]]) -> bool:
+        
+        graph=[[] for _ in range(numCourses)]
+        degree=[0 for _ in range(numCourses)]
+        for course, prereq in prerequisites:
+            graph[prereq].append(course)
+            degree[course]+=1
 
-    Suggested structure:
-      class Solution:
-          def method(...): ...
-    Then expose a tiny solve() or leave only the class and use tests to drive.
-    """
-    pass
+        queue=[i for i in range(numCourses) if degree[i]==0]
+
+        taken=0
+
+        while queue:
+            current=queue.pop(0)
+            taken+=1
+            for course in graph[current]:
+                degree[course]-=1
+                if degree[course]==0:
+                    queue.append(course)
+            
+        return taken==numCourses
+
+def solve(*args, **kwargs):
+    sol=Solution()
+    prerequisites=[[0,1]]
+    numCourses=2
+    if sol.canFinish(numCourses,prerequisites):
+        print(f"These {numCourses} courses with this prerequisites {prerequisites} can be finished")
+    else:
+        print(f"These {numCourses} courses with this prerequisites {prerequisites} can not be finished")
 
 if __name__ == "__main__":
-    print("Implement me!")
+    solve()
