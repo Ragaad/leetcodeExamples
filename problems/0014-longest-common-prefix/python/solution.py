e35e5ffc55358cc48f22e7426324abb62cff5c19
mahy777-bit <maha.abdelraosul@gmail.com>
2025-11-20 11:44:50 -0800

    Python Solution and Test for Longest Common Prefix

--- problems/0014-longest-common-prefix/python/solution.py
+++ problems/0014-longest-common-prefix/python/solution.py
@@ -1,12 +1,24 @@
-# TODO: implement the Python version for this problem.
-
+from typing import List
 class Solution:
-    def placeholder(self):
-        pass
+    def longestCommonPrefix(self, strs: List[str]) -> str:
+        if len(strs)<2:
+            return strs[0]
 
+        common_prefix=strs[0]
+        for i in range(1,len(strs)):
+            max_len=min(len(common_prefix),len(strs[i]))
+            end_index=0
+            while end_index<max_len and common_prefix[end_index]==strs[i][end_index]:
+               end_index+=1
+            common_prefix=common_prefix[:end_index]
+            if not common_prefix: 
+                break
+
+        return common_prefix
 def solve():
-    # optional runner entrypoint
-    pass
+    sol=Solution()
+    strs=["flower","flow","flight"]
+    print(f"The longest common prefix for {strs} is {sol.longestCommonPrefix(strs)}")
 
 if __name__ == "__main__":
-    print("Implement me!")
+    solve()
--- problems/0014-longest-common-prefix/python/test_solution.py
+++ problems/0014-longest-common-prefix/python/test_solution.py
@@ -1,3 +1,25 @@
-# pytest skeleton. Replace with real tests.
-def test_placeholder():
-    assert True
+import unittest
+from solution import Solution
+class TestMergedIntervals(unittest.TestCase):
+    def setUp(self):
+        self.sol=Solution()
+    
+    def check(self,strs,expected):
+        got=self.sol.longestCommonPrefix(strs)
+        self.assertEqual(got,expected,f"For the heights {strs} got {got} but expected {expected}")
+        return got == expected
+    
+    def testExamples(self):
+        tests=[
+            (["flower","flow","flight"],"fl"),
+            (["dog","racecar","car"],"")
+        ]
+
+        passed=0
+        for test in tests:
+            if self.check(test[0],test[1]):
+                passed+=1
+        print(f"\nSummary: {passed}/{len(tests)} ")
+
+if __name__=="__main__":
+    unittest.main()
