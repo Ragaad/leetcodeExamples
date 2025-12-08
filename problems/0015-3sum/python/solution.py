b1c618ec440e20145756a1d7fbea218ea054cbbf
mahy777-bit <maha.abdelraosul@gmail.com>
2025-12-08 13:59:57 -0800

    Update solution.py

--- problems/0015-3sum/python/solution.py
+++ problems/0015-3sum/python/solution.py
@@ -1,8 +1,36 @@
 # TODO: implement the Python version for this problem.
 
 class Solution:
-    def placeholder(self):
-        pass
+    def threeSum(self, nums: List[int]) -> List[List[int]]:
+       
+
+        n=len(nums)
+        nums.sort()
+        
+
+        
+        results=[]
+        for i in range(n-2):
+            if i>0 and nums[i]==nums[i-1]: 
+                continue
+            l=i+1
+            r=n-1
+            while l<r:
+                sum=nums[i]+nums[l]+nums[r]
+                if sum>0:
+                    r-=1
+                elif sum<0:
+                    l+=1
+                else:
+                    results.append([nums[i],nums[l],nums[r]])
+                    while l<r and nums[l]==nums[l+1]:
+                        l+=1
+                    while l<r and nums[r]==nums[r-1]:
+                        r-=1
+                    l+=1
+                    r-=1
+
+        return results
 
 def solve():
     # optional runner entrypoint
